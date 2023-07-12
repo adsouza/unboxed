8f0857642fbda9fd2fc261ac7ad5bfac11d785e4
Antonio D'souza <275832+adsouza@users.noreply.github.com>
2023-07-12 15:48:41 -0400

    Abstract out the querying/rendering into a function.

--- server.go
+++ server.go
@@ -18,13 +18,14 @@ func reportError(statusCode int, msg string, w http.ResponseWriter) {
 
 func reqHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+	case http.MethodGet:
+		fallthrough
 	default:
 		reportError(http.StatusMethodNotAllowed, fmt.Sprintf("Unsupported HTTP method %s.", r.Method), w)
 	}
 }
 
-func main() {
-	arg := os.Args[1]
+func render(arg string) string {
 	sql, err := os.ReadFile(arg + ".sql")
 	cmd := exec.Command("sqlite3", "-header", "-html", "-readonly", "music.sqlite3")
 	var stdout bytes.Buffer
@@ -38,8 +39,13 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("SQL execution failed with error: %s\n", err)
 	}
-	output := string(stdout.Bytes())
-	fmt.Printf("<!DOCTYPE html><html>\n<table>\n%s</table>\n</html>\n", output)
+	results:= string(stdout.Bytes())
+	return fmt.Sprintf("<!DOCTYPE html><html>\n<table>\n%s</table>\n</html>\n", results)
+}
+
+func main() {
+	arg := os.Args[1]
+	fmt.Println(render(arg))
 	http.HandleFunc("/", reqHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
