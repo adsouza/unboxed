7698b985fac51fcc09d78e71eebafc2143a292ae
Antonio D'souza <275832+adsouza@users.noreply.github.com>
2023-07-12 16:17:41 -0400

    Use URL path as filename of SQL script to run.

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func reportError(statusCode int, msg string, w http.ResponseWriter) {
@@ -19,7 +20,18 @@ func reportError(statusCode int, msg string, w http.ResponseWriter) {
 func reqHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		fallthrough
+		page := strings.TrimPrefix(r.URL.Path, "/")
+		switch page {
+		case "":
+			page = "all"
+		case "favicon.ico":
+			w.WriteHeader(http.StatusNotFound)
+			return
+		default:
+			reportError(http.StatusNotFound, fmt.Sprintf("Unrecognized path: %s.", page), w)
+			return
+		}
+		fmt.Fprintln(w, render(page))
 	default:
 		reportError(http.StatusMethodNotAllowed, fmt.Sprintf("Unsupported HTTP method %s.", r.Method), w)
 	}
@@ -27,6 +39,9 @@ func reqHandler(w http.ResponseWriter, r *http.Request) {
 
 func render(arg string) string {
 	sql, err := os.ReadFile(arg + ".sql")
+	if err != nil {
+		log.Fatal(err)
+	}
 	cmd := exec.Command("sqlite3", "-header", "-html", "-readonly", "music.sqlite3")
 	var stdout bytes.Buffer
 	cmd.Stdout = &stdout
@@ -39,13 +54,12 @@ func render(arg string) string {
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("SQL execution failed with error: %s\n", err)
 	}
-	results:= string(stdout.Bytes())
+	results := string(stdout.Bytes())
 	return fmt.Sprintf("<!DOCTYPE html><html>\n<table>\n%s</table>\n</html>\n", results)
 }
 
 func main() {
-	arg := os.Args[1]
-	fmt.Println(render(arg))
+	fmt.Println(render("all"))
 	http.HandleFunc("/", reqHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
