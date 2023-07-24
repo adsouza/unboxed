959a2d1f08913aaf96eb0958b936389b212d5b24
Antonio D'souza <275832+adsouza@users.noreply.github.com>
2023-07-24 14:55:50 -0400

    Create init.sh
    Add a shell script to create SQL scripts if necessary.

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+touch best-selling-song-over-20m-per-artist.sql
+touch hits-from-odd-numbered-years.sql
+touch mean-sales-by-year-in-rev-chron.sql
+touch songs-by-musicians-with-no-other-hits.sql
+touch songs-whose-title-contains-you.sql
+touch songs-with-sales-over-20m.sql
+touch top5-musicians-by-total-sales.sql
